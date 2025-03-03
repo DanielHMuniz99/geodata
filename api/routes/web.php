644e88c9281742d32873baf053d5c8efc7704cba
muniz <muniz@daniel.muniz>
2025-03-03 20:27:57 -0300

    api census request

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\ApiCensusController;
 
 Route::prefix('census')->group(function () {
-    Route::get('names/{name}', [ApiController::class, 'getNames']);
-    Route::get('ranking', [ApiController::class, 'getNamesByRanking']);
+    Route::get('names/{name}', [ApiCensusController::class, 'getNames']);
+    Route::get('ranking', [ApiCensusController::class, 'getNamesByRanking']);
 });

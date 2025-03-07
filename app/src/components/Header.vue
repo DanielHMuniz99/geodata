5fd03d413bd4ede2e8be3c16ee5d767d775c8c1a
muniz <muniz@daniel.muniz>
2025-03-07 11:58:15 -0300

    link to main page

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="navbar">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">API</a>
+      <a class="navbar-brand" href="/">API</a>
     </div>
   </header>
 </template>

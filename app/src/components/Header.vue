deaf8a5114831cb9a8a741a9ced075adbbb9e978
muniz <daniel.muniz997@gmail.com>
2025-03-10 14:17:17 -0300

    select locale menu

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,16 +1,17 @@
 <template>
-<v-app-bar
-    color="teal-darken-4"
-  >
+  <v-app-bar color="teal-darken-4">
     <template v-slot:image>
-      <v-img
-        gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
-      ></v-img>
+      <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
     </template>
     <template v-slot:prepend>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
     </template>
     <v-app-bar-title>API</v-app-bar-title>
     <v-spacer></v-spacer>
+    <LanguageDropdown />
   </v-app-bar>
 </template>
+
+<script setup>
+  import LanguageDropdown from './LanguageDropdown.vue';
+</script>
---
+++ app/src/components/LanguageDropdown.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-menu offset-y>
+    <template v-slot:activator="{ props }">
+      <v-btn icon v-bind="props">
+        <v-avatar size="32">
+          <v-img :src="currentFlag" alt="Language" />
+        </v-avatar>
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item v-for="lang in languages" :key="lang.code" @click="changeLanguage(lang)">
+        <v-list-item-avatar>
+          <v-img :src="lang.flag" alt="Flag" />
+        </v-list-item-avatar>
+        <v-list-item-title>{{ lang.label }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useI18n } from 'vue-i18n';
+
+const i18n = useI18n({ useScope: 'global' });
+
+const languages = ref([
+  { code: 'pt', label: 'Português', flag: '/brazil.png' },
+  { code: 'en', label: 'English', flag: '/usa.png' }
+]);
+
+const currentLanguage = ref('pt');
+const currentFlag = ref(languages.value.find(lang => lang.code === currentLanguage.value).flag);
+
+const changeLanguage = (lang) => {
+  i18n.locale.value = lang.code;
+  currentLanguage.value = lang.code;
+  currentFlag.value = lang.flag;
+};
+</script>

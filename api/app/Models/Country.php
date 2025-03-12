8674f86d1f51fa416418f5ba130ea84050b777ab
muniz <daniel.muniz997@gmail.com>
2025-03-11 21:06:15 -0300

    models income distribution

--- api/app/Models/Country.php
+++ api/app/Models/Country.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Country extends Model {
 
     protected $table = 'countries';
 
+    public function gdp()
+    {
+        return $this->hasOne(CountryGdp::class);
+    }
 }

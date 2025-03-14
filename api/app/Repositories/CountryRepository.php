c2dd3e28db5fb20213b488ec8df34637f93fb987
muniz <daniel.muniz997@gmail.com>
2025-03-14 09:18:11 -0300

    country model

--- api/app/Repositories/CountryRepository.php
+++ api/app/Repositories/CountryRepository.php
@@ -6,22 +6,29 @@ use App\Models\Country;
 
 class CountryRepository
 {
+    public $countryModel;
+
+    public function __construct(Country $countryModel)
+    {
+        $this->countryModel = $countryModel;
+    }
+
     /**
      * @param string $code
      */
     public function findByCode(string $code)
     {
-        return Country::where('code', $code)->first();
+        return $this->countryModel->where('code', $code)->first();
     }
 
     public function getAll()
     {
-        return Country::all();
+        return $this->countryModel->all();
     }
 
     public function getCountriesWithData()
     {
-        return Country::select('countries.id', 'countries.country', 'countries.code')
+        return $this->countryModel->select(['countries.id', 'countries.country', 'countries.code'])
             ->join('income_distributions', 'income_distributions.country_id', '=', 'countries.id')
             ->join('cost_of_living', 'cost_of_living.country_id', '=', 'countries.id')
             ->distinct()

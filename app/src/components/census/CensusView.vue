7938d446b16e3a45c8a105db27101927353f0344
muniz <daniel.muniz997@gmail.com>
2025-03-14 16:41:20 -0300

    census view tab url

--- app/src/components/census/CensusView.vue
+++ app/src/components/census/CensusView.vue
@@ -18,11 +18,36 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-import NameSearch from "./NameSearch.vue";
-import RankingChart from "./RankingChart.vue";
-import { useI18n } from 'vue-i18n';
+  import { ref, watch, onMounted } from 'vue';
+  import { useI18n } from 'vue-i18n';
+  import NameSearch from './NameSearch.vue';
+  import RankingChart from './RankingChart.vue';
 
-const { t, locale } = useI18n();
-const tab = ref(null);
+  const { t } = useI18n();
+  const tab = ref(1);
+
+  const hashToTab = {
+    '#by-name': 1,
+    '#ranking': 2,
+  };
+
+  const tabToHash = {
+    1: '#by-name',
+    2: '#ranking',
+  };
+
+  onMounted(() => {
+    const hash = window.location.hash;
+    if (hash && hashToTab[hash]) {
+      tab.value = hashToTab[hash];
+    }
+  });
+
+  watch(tab, (newTab) => {
+    const newHash = tabToHash[newTab];
+    if (newHash) {
+      window.location.hash = newHash;
+    }
+  });
+
 </script>

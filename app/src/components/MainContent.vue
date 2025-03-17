4c4bcd2ce86439c48a7fa0c49f6db1e00b392216
muniz <daniel.muniz997@gmail.com>
2025-03-17 15:22:38 -0300

    main content news

--- app/src/components/MainContent.vue
+++ app/src/components/MainContent.vue
@@ -1,44 +1,40 @@
 <template>
-  <v-card>
-    <v-container>
-      <v-row justify="center">
-        <v-carousel v-if="carouselNews.length" height="400">
-          <v-carousel-item
-            v-for="noticia in carouselNews"
-            :key="noticia.id"
-          >
-            <v-img
-              :src="'https://agenciadenoticias.ibge.gov.br/' + noticia.image"
-              cover
-            >
-              <v-container class="fill-height d-flex align-center justify-center">
-                <v-card-title class="text-white text-h5 text-center strokeme">
-                  {{ noticia.titulo }}
-                </v-card-title>
-              </v-container>
-            </v-img>
-          </v-carousel-item>
-        </v-carousel>
+  <v-row justify="center">
+    <v-carousel v-if="carouselNews.length" height="400">
+      <v-carousel-item
+        v-for="noticia in carouselNews"
+        :key="noticia.id"
+      >
+        <v-img
+          :src="'https://agenciadenoticias.ibge.gov.br/' + noticia.image"
+          cover
+        >
+          <v-container class="fill-height d-flex align-center justify-center">
+            <v-card-title class="text-white text-h5 text-center strokeme">
+              {{ noticia.titulo }}
+            </v-card-title>
+          </v-container>
+        </v-img>
+      </v-carousel-item>
+    </v-carousel>
 
-        <v-list v-if="listNews.length">
-          <v-list-item v-for="news in listNews" :key="news.id" :href="news.link" target="_blank">
-            <template v-slot:prepend>
-              <v-avatar size="64">
-                <v-img
-                  :src="'https://agenciadenoticias.ibge.gov.br/' + news.image"
-                  alt="notícia"
-                ></v-img>
-              </v-avatar>
-            </template>
-            <v-list-item-content>
-              <v-list-item-title>{{ news.titulo }}</v-list-item-title>
-              <v-list-item-subtitle>{{ news.introducao }}</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-row>
-    </v-container>
-  </v-card>
+    <v-list v-if="listNews.length">
+      <v-list-item v-for="news in listNews" :key="news.id" :href="news.link" target="_blank">
+        <template v-slot:prepend>
+          <v-avatar size="64">
+            <v-img
+              :src="'https://agenciadenoticias.ibge.gov.br/' + news.image"
+              alt="notícia"
+            ></v-img>
+          </v-avatar>
+        </template>
+        <v-list-item-content>
+          <v-list-item-title>{{ news.titulo }}</v-list-item-title>
+          <v-list-item-subtitle>{{ news.introducao }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-row>
 </template>
 
 <script setup>
@@ -66,7 +62,7 @@ const fetchNews = async () => {
       image: JSON.parse(noticia.imagens).image_intro || ''
     }));
   } catch (error) {
-    console.error('Erro ao buscar notícias:', error);
+    console.error('Error when searching for news:', error);
   }
 }
 
